2f5bf3e3673b3b0f8bd50a6e4d8ff62c5a186d1c
cristian-santos <cristiansantosti@gmail.com>
2022-05-26 20:53:26 -0300

    modal de alerta com refactoring

--- app.js
+++ app.js
@@ -40,6 +40,28 @@ class Db {
         localStorage.setItem(id, JSON.stringify(d));
         localStorage.setItem('id', id);
     }
+
+    recuperarTodosRegistros() {
+
+        // Array com as despesas
+        let despesas = Array();
+
+        let id = localStorage.getItem('id');
+
+        // Recuperando todas as despesas cadastradas
+        for(let i = 1; i <= id; i++) {
+            // Recuperar e inserir a despesa no array
+            let despesa = JSON.parse(localStorage.getItem(i));
+
+            // Existe a possibilidade de haver índices removidos
+            if(despesa === null) {
+                continue;
+            }
+
+            despesas.push(despesa);
+        }
+        return despesas;
+    }
 }
 
 
@@ -66,9 +88,30 @@ function cadastrarDespesa() {
     )
 
     if(despesa.validarDados()) {
-        db.salvar(despesa)
-        $('#sucessoGravacao').modal('show');
+        document.getElementById('titulo_modal').innerHTML = 'Registro inserido com sucesso!';
+        document.getElementById('titulo_modal_div').className = 'modal-header text-success';
+        document.getElementById('conteudo_modal').innerHTML = 'Despesa foi cadastrada com sucesso!';
+        document.getElementById('botao').innerHTML = 'Voltar';
+        document.getElementById('botao').className = 'btn btn-success';
+        db.salvar(despesa);
+
+        // Alerta de sucesso
+        $('#modalRegistraDespesa').modal('show');
     } else {
-        $('#erroGravacao').modal('show');
+        document.getElementById('titulo_modal').innerHTML = 'Erro na inclusão do registro!';
+        document.getElementById('titulo_modal_div').className = 'modal-header text-danger';
+        document.getElementById('conteudo_modal').innerHTML = 'Erro ao cadastrar a despesa!';
+        document.getElementById('botao').innerHTML = 'Voltar';
+        document.getElementById('botao').className = 'btn btn-danger';
+        
+        // Alerta de erro
+        $('#modalRegistraDespesa').modal('show');
     }
+}
+
+function carregaListaDespesas() {
+    let despesas = Array();
+    despesas = db.recuperarTodosRegistros();
+
+    console.log(despesas);
 }
